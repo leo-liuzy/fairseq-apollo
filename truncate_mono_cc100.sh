6c8b858e75f0fd6ca1ab0defe061efd3b8189247
leo-liuzy <zeyuliu2@cs.washington.edu>
2021-07-15 17:16:54 -0700

    update script for creating pilot corpus

---
+++ truncate_mono_cc100.sh
@@ -0,0 +1,85 @@
+source_dir=/scratch/zliu9986/cc100_mluna
+
+LG=en
+n_lines=55992676
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=ru
+n_lines=18931096
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=id
+n_lines=43119829
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=vi
+n_lines=45502758
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=de
+n_lines=30059179
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=fi
+n_lines=29123393
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=ja
+n_lines=13229089
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=es
+n_lines=33444309
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=fr
+n_lines=35384906
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=ko
+n_lines=28791702
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=zh-Hans
+n_lines=7120950
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=bg
+n_lines=16379807
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=th
+n_lines=12882453
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=el
+n_lines=15853577
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=ar
+n_lines=14873504
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=tr
+n_lines=19551970
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=hi
+n_lines=14351768
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=bn
+n_lines=13792639
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=ur
+n_lines=10788341
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=te
+n_lines=5952032
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
+
+LG=sw
+n_lines=12660806
+head -n $n_line ${source_dir}/${LG}.txt > ${LG}.txt
