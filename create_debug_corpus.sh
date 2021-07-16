d316dc75351da40177e8246c49a1fa1675bfb89f
leo-liuzy <zeyuliu2@cs.washington.edu>
2021-07-15 17:27:20 -0700

    update script to create debug corpus

--- create_debug_corpus.sh
+++ create_debug_corpus.sh
@@ -2,6 +2,7 @@ data_dir_root="data"
 source_dir="${data_dir_root}/raw_XLM_pilot_run_21Langs"
 # debug_dir="XLM_pilot_run_21Langs_debug"
 debug_dir="${source_dir}_debug"
+echo ${debug_dir}
 
 mono_dir="${source_dir}/monolingual"
 para_dir="${source_dir}/bilingual"
@@ -16,11 +17,35 @@ mkdir -p ${debug_mono_dir}
 cp ${mono_dir}/*.test ${debug_mono_dir}/
 cp ${mono_dir}/*.valid ${debug_mono_dir}/
 
-N_TRAIN=50000
-for f in ${mono_dir}/*.train; do
-    LG="${f%.train}"  # trim extension
+N_TEST=1000
+split=test
+echo "Create ${split} split:"
+for f in ${mono_dir}/*.${split}; do
+    LG="${f%.$split}"  # trim extension
     LG="${LG#${mono_dir}/}" # trim path to source_dir
     echo "Processing ${LG}"
-    head -n $N_TRAIN $f > ${debug_mono_dir}/${LG}.train
+    head -n $N_TEST $f > ${debug_mono_dir}/${LG}.$split
+done
+echo
+
+N_VALID=1000
+split=valid
+echo "Create ${split} split:"
+for f in ${mono_dir}/*.${split}; do
+    LG="${f%.$split}"  # trim extension
+    LG="${LG#${mono_dir}/}" # trim path to source_dir
+    echo "Processing ${LG}"
+    head -n $N_VALID $f > ${debug_mono_dir}/${LG}.$split
+done
+echo
+
+N_TRAIN=5000
+split=train
+echo "Create ${split} split:"
+for f in ${mono_dir}/*.${split}; do
+    LG="${f%.$split}"  # trim extension
+    LG="${LG#${mono_dir}/}" # trim path to source_dir
+    echo "Processing ${LG}"
+    head -n $N_TRAIN $f > ${debug_mono_dir}/${LG}.$split
 done
 
